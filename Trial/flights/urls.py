e3591501e7c2627ec1ec3f36e4eb30b733a5375b
mwebazavictor <mwebazavictorandrew@gmail.com>
2024-05-02 17:09:32 +0300

    lots of progress

--- Trial/flights/urls.py
+++ Trial/flights/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
-    
+    path("flights/index", views.index, name="index")
 ]
--- Trial/flights/views.py
+++ Trial/flights/views.py
@@ -1,3 +1,7 @@
+from . import models
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request,"flights/index.html", {"flights": models.Flight.objects.all()})
