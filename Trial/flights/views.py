59dffed888bf96f1f96056968940aca15efcf919
mwebazavictor <mwebazavictorandrew@gmail.com>
2024-05-02 21:16:07 +0300

    lots of major changes

--- Trial/flights/views.py
+++ Trial/flights/views.py
@@ -1,7 +1,11 @@
-from . import models
+from .models import Flight
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return render(request,"flights/index.html", {"flights": models.Flight.objects.all()})
+    return render(request,"flights/index.html", {"flights":Flight.objects.all()})
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {"flight":flight})
